bc8f02f5388e7b443fcdd75ed40b40f82d843736
iiotatlas <iiotatlas@gmail.com>
2020-11-27 11:37:38 -0400

    cors

--- models/server.js
+++ models/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
 const path = require('path');
+const cors = require('cors');
 
 const Sockets = require('./sockets');
 
@@ -21,6 +22,9 @@ class Server {
     //despliegue de directorio publico
     middlewares(){
         this.app.use(express.static(path.resolve(__dirname, '../public')))
+
+        //cors
+        this.app.use(cors());
     }
     
     configurarSockets() {
